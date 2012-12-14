4469da3865570d98d4dfde43343dceb9b8b0a50f
pirvallet <pirvallet@gmail.com>
2012-12-14 12:00:02 -0800

    Create AppDelegate.h

---
+++ AppDelegate.h
@@ -0,0 +1,12 @@
+//
+//  AppDelegate.h
+//  Minesweeper
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end

2f07511e5f4bbb451f58e298e98f2425d0fb39b5
pirvallet <pirvallet@gmail.com>
2012-12-14 12:02:35 -0800

    Create Grid.h
    Model file where a grid is defined : grid of buttons plus a matrix

---
+++ Grid.h
@@ -0,0 +1,20 @@
+//
+//  MinesweeperGrid.h
+//  Minesweeper2
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MinesweeperGrid : NSObject
+
+@property (strong) NSMutableArray *buttonTags;
+
+@property (nonatomic) NSMutableArray *matrix;
+@property (nonatomic) BOOL *mined;
+
+//- (id)initWithGrid:(NSMutableArray *)gridOfButtons andMatrixOfCells:(NSMutableArray *)matrix;
+- (NSMutableArray *)defineGridWithNbOfColumns:(int)nbColumns andNbOfRows:(int)nbRows;
+- (NSMutableArray *)returnTheMatrixOfCells;
+- (NSMutableArray *)returnTheTagsOfButtonsWithMine;
+
+@end
